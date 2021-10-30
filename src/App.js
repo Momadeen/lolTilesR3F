ed1eef71349b5fe91f455c664d8d25bcdb656567
Momadeen <madienxz@gmail.com>
2021-10-30 17:53:46 +0200

    created league of legends gallary tiles with react three fiber

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Image, ScrollControls, Scroll, useScroll } from "@react-three/drei";
+import { useSnapshot } from "valtio";
+// import {Minimap} from './Minimap';
+import { state, damp } from "./util";
+import { Minimap } from "./Minimap";
+
+function Item({ index, position, scale, c = new THREE.Color(), ...props }) {
+  const ref = useRef();
+  const scroll = useScroll();
+  const { clicked, urls } = useSnapshot(state);
+  const [hovered, hover] = useState(false);
+  const click = () => (state.clicked = index === clicked ? null : index);
+  const over = () => hover(true);
+  const out = () => hover(false);
+
+  console.log(clicked);
+
+  useFrame((state, delta) => {
+    const y = scroll.curve(
+      index / urls?.length - 1.5 / urls?.length,
+      4 / urls?.length
+    );
+    ref.current.material.scale[1] = ref.current.scale.y = damp(
+      ref.current.scale.y,
+      clicked === index ? 5 : 4 + y,
+      8,
+      delta
+    );
+    ref.current.material.scale[0] = ref.current.scale.x = damp(
+      ref.current.scale.x,
+      clicked === index ? 4.7 : scale[0],
+      6,
+      delta
+    );
+    if (clicked !== null && index < clicked)
+      ref.current.position.x = damp(
+        ref.current.position.x,
+        position[0] - 2,
+        6,
+        delta
+      );
+    if (clicked !== null && index > clicked)
+      ref.current.position.x = damp(
+        ref.current.position.x,
+        position[0] + 2,
+        6,
+        delta
+      );
+    if (clicked === null || clicked === index)
+      ref.current.position.x = damp(
+        ref.current.position.x,
+        position[0],
+        6,
+        delta
+      );
+    ref.current.material.grayscale = damp(
+      ref.current.material.grayscale,
+      hovered || clicked === index ? 0 : Math.max(0, 1 - y),
+      6,
+      delta
+    );
+    ref.current.material.color.lerp(
+      c.set(hovered || clicked === index ? "white" : "#aaa"),
+      hovered ? 0.3 : 0.1
+    );
+  });
+  return (
+    <Image
+      ref={ref}
+      {...props}
+      position={position}
+      scale={scale}
+      onClick={click}
+      onPointerOver={over}
+      onPointerOut={out}
+    />
+  );
+}
+
+function Items({ w = 0.7, gap = 0.15 }) {
+  const { urls } = useSnapshot(state);
+  const { width } = useThree((state) => state.viewport);
+  const xW = w + gap;
+  return (
+    <ScrollControls
+      horizontal
+      damping={6}
+      pages={(width - xW + urls.length * xW) / width}
+    >
+      <Minimap />
+      <Scroll>
+        {urls.map((url, i) => (
+          <Item
+            key={i}
+            index={i}
+            position={[i * xW, 0, 0]}
+            scale={[w, 4, 1]}
+            url={url}
+          />
+        ))}
+      </Scroll>
+    </ScrollControls>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      gl={{ antialias: false }}
+      dpr={[1, 1.5]}
+      onPointerMissed={() => (state.clicked = null)}
+    >
+      <Items />
+    </Canvas>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+      <a href="https://github.com/Momadeen?tab=repositories" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+        Mohammed Hassanien
+      </a>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>League of Legends —</div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+    <Overlay />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
